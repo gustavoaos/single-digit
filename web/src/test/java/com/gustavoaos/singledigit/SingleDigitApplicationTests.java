56bbcff07d0fac1d97161b42c957e97dcd85125b
Gustavo Alves <gustavo_alves@outlook.com>
2021-01-25 18:02:27 -0300

    chore: add spring

---
+++ web/src/test/java/com/gustavoaos/singledigit/SingleDigitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoaos.singledigit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleDigitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
