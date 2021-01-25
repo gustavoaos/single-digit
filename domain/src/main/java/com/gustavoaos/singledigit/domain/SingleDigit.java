d543981b2435d8115f4486a8293a959acfd118b1
Gustavo Alves <gustavo_alves@outlook.com>
2021-01-25 19:24:41 -0300

    test: ensures SingleDigit throws an InvalidParameterException when n is negative

---
+++ domain/src/main/java/com/gustavoaos/singledigit/domain/SingleDigit.java
@@ -0,0 +1,18 @@
+package com.gustavoaos.singledigit.domain;
+
+import java.security.InvalidParameterException;
+
+public class SingleDigit {
+
+    private final String n;
+    private final String k;
+
+    public SingleDigit(String n, String k) {
+        if (Integer.parseInt(n) < 1) {
+            throw new InvalidParameterException("N should be greater than 1");
+        }
+
+        this.n = n;
+        this.k = k;
+    }
+}
---
+++ domain/src/test/java/com/gustavoaos/singledigit/domain/SingleDigitTest.java
@@ -0,0 +1,29 @@
+package com.gustavoaos.singledigit.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.security.InvalidParameterException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+class SingleDigitTest {
+
+    String n = "";
+    String k = "";
+
+    @Test
+    @DisplayName("Should throws an Invalid Parameter Exception when N is negative")
+    void shouldThrowsInvalidParameterExceptionWhenNIsNegative() {
+        n = "-1";
+        k = "valid_value";
+
+        assertThatThrownBy(() -> new SingleDigit(n, k))
+                .isInstanceOf(InvalidParameterException.class)
+                .hasMessage("N should be greater than 1");
+
+    }
+}
