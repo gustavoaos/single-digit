747a6b1051fa53252a9059450f7d16e11c3df423
Gustavo Alves <gustavo_alves@outlook.com>
2021-01-25 19:43:48 -0300

    refactor: validate minimum value for SingleDigit parameters

--- domain/src/main/java/com/gustavoaos/singledigit/domain/SingleDigit.java
+++ domain/src/main/java/com/gustavoaos/singledigit/domain/SingleDigit.java
@@ -4,14 +4,20 @@ import java.security.InvalidParameterException;
 
 public class SingleDigit {
 
+    private static final Integer MINIMUM_VALUE = 1;
+
     private final String n;
     private final String k;
 
+    private boolean isLowerThanMinimumValue(String s) {
+        return Integer.parseInt(s) < MINIMUM_VALUE;
+    }
+
     public SingleDigit(String n, String k) {
-        if (Integer.parseInt(n) < 1) {
+        if (isLowerThanMinimumValue(n)) {
             throw new InvalidParameterException("N should be greater than 1");
         }
-        if (Integer.parseInt(k) < 1) {
+        if (isLowerThanMinimumValue(k)) {
             throw new InvalidParameterException("K should be greater than 1");
         }
 
