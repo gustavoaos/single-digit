94007a26714789552638eb758eb1d6a5d966c758
Gustavo Alves <gustavo_alves@outlook.com>
2021-01-27 00:20:40 -0300

    test: ensures UserController throws 400 bad request when no user is provided

--- api/scr/test/java/com/gustavoaos/singledigit/api/controller/UserControllerTest.java
+++ api/scr/test/java/com/gustavoaos/singledigit/api/controller/UserControllerTest.java
@@ -1,8 +1,35 @@
 package com.gustavoaos.singledigit.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gustavoaos.singledigit.application.CreateUserInteractor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Description;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CreateUserInteractor createUserInteractor;
+
+    @Test
+    @Description("Should return 400 if no user is provided")
+    void shouldReturn400IfNoUserIsProvided() throws Exception {
+        mockMvc.perform(post("/users")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(null)))
+                .andExpect(status().isBadRequest());
+    }
 }
