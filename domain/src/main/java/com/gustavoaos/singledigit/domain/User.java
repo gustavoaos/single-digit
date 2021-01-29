579145f19335bbc68e0f2a0722dfd64597fa43b3
Gustavo Alves <gustavo_alves@outlook.com>
2021-01-28 23:22:22 -0300

    feature: config entity User

--- domain/src/main/java/com/gustavoaos/singledigit/domain/User.java
+++ domain/src/main/java/com/gustavoaos/singledigit/domain/User.java
@@ -3,14 +3,23 @@ package com.gustavoaos.singledigit.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID uuid;
     private String name;
     private String email;
