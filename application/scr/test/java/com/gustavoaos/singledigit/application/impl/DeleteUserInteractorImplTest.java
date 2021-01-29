0b64795d975ab889fa1fe9d1a9b819d1f5c5f0e8
Gustavo Alves <gustavo_alves@outlook.com>
2021-01-29 16:57:16 -0300

    test: ensures delete method trows IllegalArgumentException when invalid uuid is provided

--- application/scr/test/java/com/gustavoaos/singledigit/application/impl/DeleteUserInteractorImplTest.java
+++ application/scr/test/java/com/gustavoaos/singledigit/application/impl/DeleteUserInteractorImplTest.java
@@ -1,6 +1,7 @@
 package com.gustavoaos.singledigit.application.impl;
 
 import com.gustavoaos.singledigit.domain.User;
+import com.gustavoaos.singledigit.domain.exception.NotFoundException;
 import com.gustavoaos.singledigit.domain.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Description;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -46,5 +48,31 @@ class DeleteUserInteractorImplTest {
         sut.execute(uuid.toString());
 
         verify(userRepository, times(1)).delete(user);
+    }
+
+    @Test
+    @Description("Should throw a NotFoundException when invalid id is provided")
+    void shouldThrowANotFoundExceptionWhenInValidIdIsProvided() {
+        String invalidId = uuid.toString();
+
+        when(userRepository.findById(uuid)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> sut.execute(invalidId))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("Resource not found");
+
+        verify(userRepository, times(0)).delete(user);
+    }
+
+    @Test
+    @Description("Should throw an IllegalArgumentException when invalid UUID is provided")
+    void shouldThrowAnIllegalArgumentExceptionWhenInValidUuidIsProvided() {
+        String invalidUuid = "123L";
+
+        assertThatThrownBy(() -> sut.execute(invalidUuid))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Invalid UUID string: " + invalidUuid);
+
+        verify(userRepository, times(0)).delete(user);
     }
 }
