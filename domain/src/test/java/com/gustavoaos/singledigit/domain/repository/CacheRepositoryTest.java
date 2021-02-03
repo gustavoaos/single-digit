0eee7301f8dbb4bd56ef2b64a15b27afa591b13a
Gustavo Alves <gustavo_alves@outlook.com>
2021-02-02 22:46:30 -0300

    test: ensures CacheRepository returns single digit optional when value is stored in cache

--- domain/src/test/java/com/gustavoaos/singledigit/domain/repository/CacheRepositoryTest.java
+++ domain/src/test/java/com/gustavoaos/singledigit/domain/repository/CacheRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.gustavoaos.singledigit.domain.repository;
 
 import com.gustavoaos.singledigit.application.request.ComputeSingleDigitRequest;
+import com.gustavoaos.singledigit.domain.SingleDigit;
+import com.gustavoaos.singledigit.domain.strategy.ComputeStrategy;
+import com.gustavoaos.singledigit.domain.strategy.SingleDigitStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -12,6 +15,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.util.Map;
+import java.util.Optional;
 
 @SpringBootTest
 class CacheRepositoryTest {
@@ -23,14 +27,38 @@ class CacheRepositoryTest {
     private CacheRepository sut;
 
     private ComputeSingleDigitRequest request;
+    private SingleDigit sd;
+
+    @BeforeEach
+    void initEach() {
+        SingleDigitStrategy strategy = new ComputeStrategy();
+        String n = "9875";
+        String k = "4";
+
+        request = ComputeSingleDigitRequest.builder().n(n).k(k).build();
+        sd = new SingleDigit(n, k, strategy);
+    }
 
     @Test
     @Description("Should return empty optional when value is not stored in cache")
     void shouldReturnEmptyOptionalWhenValueIsNotStoredInCache() {
-        request = ComputeSingleDigitRequest.builder().n("9875").k("4").build();
-
         when(cache.containsKey(request)).thenReturn(false);
 
         assertThat(sut.getFromCache(request)).isEmpty();
     }
+
+    @Test
+    @Description("Should return single digit optional when value is stored in cache")
+    void shouldReturnSingleDigitOptionalWhenValueIsStoredInCache() {
+        when(cache.containsKey(request)).thenReturn(true);
+        when(cache.get(request)).thenReturn(sd.getResult());
+
+        Optional<SingleDigit> response = sut.getFromCache(request);
+
+        assertThat(response).isPresent();
+        assertThat(response.get().getN()).isEqualTo(sd.getN());
+        assertThat(response.get().getK()).isEqualTo(sd.getK());
+        assertThat(response.get().getResult()).isEqualTo(sd.getResult());
+    }
+
 }
