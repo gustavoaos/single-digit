21a4258b352f2eabae3626bf66cd8579d9a6485c
Gustavo Alves <gustavo_alves@outlook.com>
2021-02-05 12:33:18 -0300

    docs: define api reponses on user controller

--- api/scr/main/java/com/gustavoaos/singledigit/api/controller/UserController.java
+++ api/scr/main/java/com/gustavoaos/singledigit/api/controller/UserController.java
@@ -6,12 +6,16 @@ import com.gustavoaos.singledigit.application.request.ComputeSingleDigitRequest;
 import com.gustavoaos.singledigit.application.request.UpdateUserRequest;
 import com.gustavoaos.singledigit.application.response.SingleDigitListResponse;
 import com.gustavoaos.singledigit.application.response.UserResponse;
-import com.gustavoaos.singledigit.domain.exception.WrongKeyException;
+import com.gustavoaos.singledigit.domain.error.ApiError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -43,13 +47,21 @@ public class UserController {
         this.listSingleDigitsInteractor = listSingleDigitsInteractor;
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody ResponseEntity<UserResponse> create(@Valid @RequestBody CreateUserRequest request) {
-        UserResponse user = this.createUserInteractor.execute(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
-    }
-
+    @Operation(summary = "Get user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found user",
+                content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = UserResponse.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid arguments provided",
+                content = {  @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ApiError.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                content = {  @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ApiError.class))
+            })
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody ResponseEntity<UserResponse> find(
@@ -64,12 +76,56 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
+    @Operation(summary = "Create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created user",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponse.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid arguments provided",
+                    content = {  @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ApiError.class))
+            })
+    })
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody ResponseEntity<UserResponse> create(@Valid @RequestBody CreateUserRequest request) {
+        UserResponse user = this.createUserInteractor.execute(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @Operation(summary = "Delete user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deleted user",
+                content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = UserResponse.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                content = {  @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ApiError.class))
+            })
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") String id) {
         this.deleteUserInteractor.execute(id);
     }
 
+    @Operation(summary = "Update user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated user",
+                content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = UserResponse.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid arguments provided",
+                content = {  @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ApiError.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                content = {  @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ApiError.class))
+            })
+    })
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody ResponseEntity<UserResponse> update(
@@ -79,6 +135,21 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
+    @Operation(summary = "Compute single digit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Computed single digit",
+                content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = Integer.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid arguments provided",
+                content = {  @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ApiError.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                content = {  @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ApiError.class))
+            })
+    })
     @GetMapping("/compute")
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody ResponseEntity<Integer> compute(
@@ -93,6 +164,17 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(sd);
     }
 
+    @Operation(summary = "Get single digit list for user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Computed single digit list",
+                content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = SingleDigitListResponse.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                content = {  @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ApiError.class))
+            })
+    })
     @GetMapping("/{id}/list")
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody ResponseEntity<SingleDigitListResponse> list(
--- api/scr/main/java/com/gustavoaos/singledigit/api/controller/exception/ExceptionHandlerController.java
+++ api/scr/main/java/com/gustavoaos/singledigit/api/controller/exception/ExceptionHandlerController.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
 
     @Override
